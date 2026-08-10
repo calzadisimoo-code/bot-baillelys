9181f08d38e50d66373ffdf03025c82bfd8d3a03
Camilo <calzadisimoo@gmail.com>
2026-08-10 00:09:19 -0500

    2.0

--- respuestas/carg120w.js
+++ respuestas/carg120w.js
@@ -9,7 +9,6 @@ module.exports = function (texto, usuario) {
     if (
         texto.includes("120w") ||
         texto.includes("120 w") ||
-		texto.includes("quiero el cargador de 120w") ||
         texto.includes("cargador 120") ||
         texto.includes("cargador de 120")
     ) {
@@ -23,17 +22,17 @@ module.exports = function (texto, usuario) {
 
 return obtenerVariante("carg120w", usuario, {
 
-    A: "⚡ ¡Sí disponible!\n\n💰 Precio: *$45.000*\n\n📍 ¿Envío o recoges?",
+    A: "⚡ ¡Sí disponible!\n\n💰 Precio: *$60.000*\n\n📍 ¿Envío o recoges?",
 
-    B: "⚡ El cargador Turbo *120W* carga mucho más rápido que uno convencional.\n\n💰 Solo *$45.000*.\n\n📍 ¿Lo necesitas para envío o recoger?",
+    B: "Listo, prefieres recoger en el local o envio?.\n\n📍 Dime tu barrio y te digo cómo te la hacemos llegar",
 
-    C: "🔥 Es uno de los cargadores que más vendemos por su velocidad de carga.\n\n💰 *$45.000*.\n\n📍 ¿Te lo enviamos o pasas por él?",
+    C: "🔥 Es uno de los cargadores que más vendemos por su velocidad de carga.\n\n💰 *$60.000*.\n\n📍 ¿Te lo enviamos o pasas por él?",
 
-    D: "👋 ¡Hola! Claro que sí.\n\n¿Para qué celular lo necesitas?\n\n💰 El cargador Turbo *120W* vale *$45.000*.",
+    D: "👋 ¡Hola! Claro que sí.\n\n¿Para qué celular lo necesitas?\n\n💰 El cargador Turbo *120W* vale *$60.000*.",
 
     E: "✅ Sí tenemos disponible el cargador Turbo *120W*.\n\n🚚 Envío rápido a toda Colombia.\n💰 *$45.000*.\n\n📍 ¿De qué ciudad nos escribes?",
 
-    F: "⚡ ¡Perfecto!\n\n💰 Cargador Turbo *120W*: *$45.000*.\n\n😊 ¿Cuántas unidades necesitas?"
+    F: "⚡ ¡Perfecto!\n\n💰 Cargador Turbo *180W*: *$60.000*.\n\n😊 ¿Cuántas unidades necesitas?"
 
 });
 
---
+++ respuestas/carg180w.js
@@ -0,0 +1,44 @@
+const { obtenerVariante } = require("../estadisticas/ab");
+const { guardar } = require("../estado");
+const {
+    registrarProducto
+} = require("../estadisticas/hoy");
+
+module.exports = function (texto, usuario) {
+
+    if (
+        texto.includes("180w") ||
+        texto.includes("180 w") ||
+		texto.includes("quiero el cargador de 120w") ||
+        texto.includes("cargador 180") ||
+        texto.includes("cargador de 180")
+    ) {
+	
+        registrarProducto(usuario);
+
+        guardar(usuario, {
+            producto: "carg120w",
+			pedidoEnviado: false
+        });
+
+return obtenerVariante("carg120w", usuario, {
+
+    A: "⚡ ¡Sí disponible!\n\n💰 Precio: *$45.000*\n\n📍 ¿Envío o recoges?",
+
+    B: "⚡ El cargador Turbo *120W* carga mucho más rápido que uno convencional.\n\n💰 Solo *$45.000*.\n\n📍 ¿Lo necesitas para envío o recoger?",
+
+    C: "🔥 Es uno de los cargadores que más vendemos por su velocidad de carga.\n\n💰 *$45.000*.\n\n📍 ¿Te lo enviamos o pasas por él?",
+
+    D: "👋 ¡Hola! Claro que sí.\n\n¿Para qué celular lo necesitas?\n\n💰 El cargador Turbo *120W* vale *$45.000*.",
+
+    E: "✅ Sí tenemos disponible el cargador Turbo *120W*.\n\n🚚 Envío rápido a toda Colombia.\n💰 *$45.000*.\n\n📍 ¿De qué ciudad nos escribes?",
+
+    F: "⚡ ¡Perfecto!\n\n💰 Cargador Turbo *120W*: *$45.000*.\n\n😊 ¿Cuántas unidades necesitas?"
+
+});
+
+    }
+
+    return null;
+
+}
