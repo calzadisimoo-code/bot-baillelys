660eb3d96d64b0a2db09ea9de0759ae061133521
Camilo <calzadisimoo@gmail.com>
2026-08-18 10:21:25 -0500

    2.0

--- respuestas/complejodemagnesio.js
+++ respuestas/complejodemagnesio.js
@@ -22,8 +22,10 @@ module.exports = function (texto, usuario) {
         });
 
         return obtenerVariante("complejodemagnesio", usuario, {
+
             A:
-                "¡Sí tenemos disponible el Magencio Complex 1000 mg!\n\n $89.990\n?? 90 cápsulas — 8 formas de magencio.\n Envíos a todo Colombia.\n\n La promoción está activa por tiempo limitado. Si quieres aprovecharla, te lo puedo dejar separado hoy mismo.\n\n Pásame nombre completo, ciudad, dirección y número de teléfono y te dejo el pedido listo"
+                "🔥 ¡Sí tenemos disponible el Magencio Complex 1000 mg!\n\n💰 $89.990\n💊 90 cápsulas — 8 formas de magencio.\n🚚 Envíos a todo Colombia.\n\n⚡ La promoción está activa por tiempo limitado.\n\n📍 Pásame nombre completo, ciudad, dirección y número de teléfono y te dejo el pedido listo"
+
         });
 
     }
