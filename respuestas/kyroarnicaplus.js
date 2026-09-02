2e2a565e09ae063734d5dbe8af4fc3b677c6d023
Camilo <calzadisimoo@gmail.com>
2026-09-02 14:44:16 -0500

    2.0

--- respuestas/kyroarnicaplus.js
+++ respuestas/kyroarnicaplus.js
@@ -1,4 +1,3 @@
-
 const { obtenerVariante } = require("../estadisticas/ab");
 const { guardar } = require("../estado");
 
@@ -19,11 +18,15 @@ module.exports = function (texto, usuario) {
             pedidoEnviado: false
         });
 
-        return obtenerVariante("kyroarnicaplus", usuario, {
+        return {
+            foto: true,
+            producto: "kyroarnicaplus",
+            texto: obtenerVariante("kyroarnicaplus", usuario, {
 
-            A: "La promoción de Kyro Árnica Plus x2 (250ML c/u) sigue disponible por $89.900 con envío *GRATIS*.\n\nPago *Contra entrega* 🚚.\n\n✅ Ayuda a aliviar dolores musculares y articulares, espalda, hombros, rodillas, cuello y tensión muscular, brindando una sensación calmante y mayor movilidad.\n\nPara validar la entrega y dejar tu pedido registrado, indícame por favor:\n\n✅ Nombre completo\n\n✅ Dirección de entrega\n\nTu pedido llega entre 1 y 3 días hábiles."
+                A: "La promoción de Kyro Árnica Plus x2 (250ML c/u) sigue disponible por $89.900 con envío *GRATIS*.\n\nPago *Contra entrega* 🚚.\n\n✅ Ayuda a aliviar dolores musculares y articulares, espalda, hombros, rodillas, cuello y tensión muscular, brindando una sensación calmante y mayor movilidad.\n\nPara validar la entrega y dejar tu pedido registrado, indícame por favor:\n\n✅ Nombre completo\n\n✅ Dirección de entrega\n\nTu pedido llega entre 1 y 3 días hábiles."
 
-        });
+            })
+        };
 
     }
 
