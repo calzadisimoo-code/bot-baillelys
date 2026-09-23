c029f8935c1caa29105117ab7c8f8e4834333ada
Camilo <calzadisimoo@gmail.com>
2026-09-23 10:17:13 -0500

    2.0

--- respuestas/pro4.js
+++ respuestas/pro4.js
@@ -88,7 +88,26 @@ Para despacharlos hoy mismo envíame:
 ✅ ¿Cuál prefieres?`
                     }
                 ]
-            }
+            },
+			C: {
+    mensajes: [
+        {
+            foto: true,
+            producto: "pro4",
+            imagen: 1,
+            texto: "🎧 AirPods Pro 2"
+        },
+        {
+            foto: true,
+            producto: "pro4",
+            imagen: 2,
+            texto: "🎧 AirPods Pro 4"
+        },
+        {
+            texto: `¿Cuál prefieres?`
+        }
+    ]
+}
 
         });
 
